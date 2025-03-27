fdb25362897a66d865f977b781f1503836a38241
nirokay <nirokay-public@protonmail.com>
2025-03-27 11:20:11 +0100

    started automatic generation of elements and such things

---
+++ cattag/auto/generator/all.nim
@@ -0,0 +1,4 @@
+import
+    htmlElements
+export
+    htmlElements
--- cattag/auto/generator/parser.nim
+++ cattag/auto/generator/parser.nim
@@ -2,10 +2,22 @@ import std/[os, strutils]
 
 const resourceDirectory: string = "resources"
 
+
+type OutputFile* = object
+    filename*: string
+    lines*: seq[string]
+
+proc newOutputFile*(filename: string): OutputFile = OutputFile(
+    filename: filename
+)
+proc writeFile*(file: OutputFile) =
+    writeFile(".." / file.filename, file.lines.join("\n"))
+
+
 proc fileContent(file: string): string =
     result = readFile(resourceDirectory / file)
-
 proc parseFileLines*(file: string): seq[string] =
+    ## Parses file (skips empty lines and comments)
     var content: string = file.fileContent()
     for line in content.split("\n"):
         let formattedLine: string = line.strip()
